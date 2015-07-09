602a4d66114166b15b1fc8d9fe0a69356157c057
punchmonster <igloman88@gmail.com>
2015-07-09 11:09:40 +0200

    post loading from DB on index complete

--- app.lua
+++ app.lua
@@ -9,37 +9,15 @@ app:enable('etlua')
 app.layout = require 'views.layout'
 
 app:get('/', function(self)
-	local content = nil
+	-- retrieve posts table rowcount
+	local row_count   = db.select("COUNT(*) from posts" )
+	-- retrieve post list
+	self.table_content = db.select("* from posts where postID >= 1 and postID <= ? order by postID DESC", row_count[1]['COUNT(*)'] )
 
-	function readFile(file, i)
-		-- assign file to variable
-	    local f = io.open(file, 'rb')
-	    -- check if file exists, if not 404
-	    if f~=nil then
-	    	content = f:read('*all')
-	    	f:close()
-	    	return content
-	    else
-	    	return false
-	    end
-	end
-	
-	-- array to store the posts in
-	self.post_list = {}
-
-	-- for loop to insert posts into array
-	for i=1, 11, 1 do
-		-- check if there's a post to retrieve
-		if readFile('posts/post' .. i .. '.txt', i) ~= false then
-			-- read the whole file into a string
-			self.post_list[i] = readFile('posts/post' .. i .. '.txt', i) .. '<div class="col-50">&nbsp;</div><div class="col-50" ><a href="/post/' .. i .. '" class="read-more">read more...</a></div>'
-		else
-			break
-		end
-	end
+	--self.post_content = row_count[1]['COUNT(*)']
 
 	-- set page title
-	self.page_title = '{ home }'
+	self.page_title = 'Izana - home'
 
 	return { render = 'index'}
 end)
