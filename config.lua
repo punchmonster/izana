d4e6ca5e8b886b36e5353ec53db4656e18347508
punchmonster <igloman88@gmail.com>
2015-07-25 10:09:59 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,14 +6,14 @@ config("development", {
 })
 
 config("development", {
-  secret = "rainbownigger"
+  secret = "secret"
 })
 
 config("development", {
   mysql = {
     host = "127.0.0.1",
     user = "root",
-    password = "poopdick",
+    password = "password",
     database = "izanaDB"
   }
 })
